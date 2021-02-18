6277203441bb51a391b26b2c2308a905cc88a371
DongHeon Lee <niche3600@gmail.com>
2021-02-19 07:19:31 +0900

    Todoが何もないときの表示を追加

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { TodoListItem } from './TodoListItem';
 
 interface Props {
@@ -7,15 +7,31 @@ interface Props {
 }
 
 export const TodoList: React.FC<Props> = ({ todos, toggleTodo }) => {
+
+  const generateTodoList = (todos: Todo[]) => {
+    if (todos.length < 1) {
+      return (
+        <div>
+          There's nothing to Todo
+        </div>
+      )
+    }
+
+    return (
+      <ul>
+        {todos.map(todo => (
+          <TodoListItem
+            key={todo.text}
+            todo={todo}
+            toggleTodo={toggleTodo}
+          />
+        ))}
+      </ul>
+    )
+  }
   return (
-    <ul>
-      {todos.map(todo => (
-        <TodoListItem
-          key={todo.text}
-          todo={todo}
-          toggleTodo={toggleTodo}
-        />
-      ))}
-    </ul>
+    <Fragment>
+      { generateTodoList(todos)}
+    </Fragment>
   );
 }
